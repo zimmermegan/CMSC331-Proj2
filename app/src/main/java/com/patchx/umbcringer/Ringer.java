943c16866abba888d30923930ec522607a44f14d
Frank Zastawnik <frankz2@umbc.edu>
2014-12-03 22:25:00 -0500

    Moved some things around.  Added courses to the buttons.

--- app/src/main/java/com/patchx/umbcringer/Ringer.java
+++ app/src/main/java/com/patchx/umbcringer/Ringer.java
@@ -1,7 +1,39 @@
 package com.patchx.umbcringer;
 
-/**
- * Created by frank on 12/3/14.
- */
-public class Ringer {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class Ringer extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ringer);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_ringer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }

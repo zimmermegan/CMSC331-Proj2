35cb512649d6369a7fec191218939f7dded53248
Frank Zastawnik <frankz2@umbc.edu>
2014-12-04 10:01:18 -0500

    FZ - 12/04/2014 - Working on alarm scheduler

---
+++ app/src/main/java/com/patchx/umbcringer/AlarmReceiver.java
@@ -0,0 +1,19 @@
+package com.patchx.umbcringer;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by frank on 12/4/14.
+ */
+public class AlarmReceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+
+    }
+
+}
